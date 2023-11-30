fe482031cc53e47c2139b538227786533200b7d6
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-30 19:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+	const fnameInput = document.getElementById('fname');
+	 fnameInput.addEventListener('blur',function () {
+		 fnameInput.value = fnameInput.value.toUpperCase();
+	 });
+});
